8f82777c07f4f5b23d9e79101f21c46f7b9ab78c
Byunsangjin <qustkdwls1@naver.com>
2019-06-17 10:00:06 +0900

    시간 기록 테이블 생성

---
+++ ObjcTimerProject/TimeTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  TimeTableViewCell.h
+//  ObjcTimerProject
+//
+//  Created by Byunsangjin on 17/06/2019.
+//  Copyright © 2019 Byunsangjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TimeTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *timeRecord;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjcTimerProject/TimeTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TimeTableViewCell.m
+//  ObjcTimerProject
+//
+//  Created by Byunsangjin on 17/06/2019.
+//  Copyright © 2019 Byunsangjin. All rights reserved.
+//
+
+#import "TimeTableViewCell.h"
+
+@implementation TimeTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
